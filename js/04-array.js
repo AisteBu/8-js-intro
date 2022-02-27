c29ba6ef68d6004c0f30af32fc0386f794487b21
AisteB <aistebuneviciene@gmail.com>
2022-02-27 15:00:44 +0100

    array methods

--- js/04-array.js
+++ js/04-array.js
@@ -1,5 +1,12 @@
 /*
 ARRAY
+
+array metodai (funkcionalumas):
+- .push()       - prideti i gala nauja reiksme
+- .unshift()    - prideti i preiki nauja reiksme
+- .pop()        - isimti is galo (paskutine) reiksme
+- .shift()      - issimti is priekio (pirma) reiksme
+
 */
 
 const marks = [10, 2, 8, 4, 6, 5, 8];
@@ -17,3 +24,37 @@ console.log('paskutinis:', last);
 const dictonary = ['labas', 'rytas', 'sakau', 'tau', '*'];
 const paskutinisZodis = dictonary[dictonary.length - 1];
 console.log(paskutinisZodis);
+
+console.log('----------------');
+
+const abc = ['b', 'c'];
+console.log(abc);
+
+abc.push('d');
+console.log(abc);
+
+abc.shift();
+console.log(abc);
+
+abc.pop();
+console.log(abc);
+
+abc.unshift('a');
+console.log(abc);
+
+console.log(abc.reverse());
+console.log(abc);
+
+console.log('----------------');
+
+console.log(['a', 'b'].concat(['c', 'd']));
+
+const a1 = ['a', 'b'];
+const a2 = ['c', 'd']
+console.log(a1.concat(a2));
+
+const modern1 = [...a1, ...a2];
+console.log(modern1);
+
+const modern2 = [...a2, ...a1];
+console.log(modern2);
