15dcb41aab31f3b36fd8f08f03c009bc63bc4554
AisteB <aistebuneviciene@gmail.com>
2022-02-28 21:45:21 +0100

    IF

---
+++ js/06-if.js
@@ -0,0 +1,75 @@
+/*
+IF - salygos sakinys
+
+PALYGINIMO OPERATORIAI:
+visi: >, <, >=, <=, ==, !=, ===, !==
+nenaudotini: ==, !=
+naudotini: >, <, >=, <=, ===, !==
+
+SABLONAI:
+if () {}
+if () {} else {}
+if () {} else if () {}
+if () {} else if () {} ... else if () {}
+if () {} else if () {} ... else if () {} else {}
+
+*/
+const a = 1998765;
+
+if (a) {
+    console.log('taip');
+} else {
+    console.log('ne');
+}
+
+
+const akys = 'zalios';
+
+if (akys === 'melynos') {
+    console.log('Melynakis');
+} else {
+    console.log('Nezinoma akiu spalva');
+}
+
+
+const akys1 = 'rudos';
+
+if (akys1 === 'melynos') {
+    console.log('Melynakis');
+} else if (akys1 === 'zalios') {
+    console.log('Zaliaakis');
+} else if (akys1 === 'rudos') {
+    console.log('Rudaakis');
+} else {
+    console.log('Neatpazinta akiu spalva');
+}
+
+console.log('---------------------');
+
+const parosMetas = 'vakaras';
+const arLyja = true;
+
+if (parosMetas === 'rytas') {
+    if (arLyja) {
+    console.log('Labas rytas, nepamirsk skecio!');
+} else {
+    console.log('Labas rytas!'); 
+}
+} else {
+    if (parosMetas === 'diena') {
+        if (arLyja) {
+            console.log('Laba diena, nepamirsk skecio!');
+        } else {
+            console.log('Laba diena!'); 
+        }
+        } else {
+        if (parosMetas === 'vakaras') {
+            if (arLyja) {
+                console.log('Labas vakaras, nepamirsk skecio!');
+            }
+         } else {
+                console.log('Neatpazintas paros metas'); 
+         }
+     } 
+   
+}
